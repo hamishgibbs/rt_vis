630fe49650a053232a6a397b405e526b4034e850
hamishgibbs <hamishgibbs@gmail.com>
2020-09-02 07:56:17 +0100

    legend animation

--- dist/build.js
+++ dist/build.js
@@ -504,27 +504,28 @@ var map = (function (_super) {
             .style('stroke-width', '0.5px');
     };
     map.prototype.createLegend = function (map_svg, map_svg_dims, colour_ref) {
-        console.log(map_svg_dims);
         var legend_height = 200;
         var legend_x = map_svg_dims.width / 30;
         var legend_y = map_svg_dims.height / 2;
         var legendClick = function (x) {
-            console.log(this);
-            console.log(d3.selectAll('#map-legend-text').style('opacity'));
-            console.log(typeof (d3.selectAll('#map-legend-text').style('opacity')));
-            console.log(d3.selectAll('#map-legend-text').style('opacity') === '1');
             if (d3.selectAll('#map-legend-text').style('opacity') === '1') {
                 d3.selectAll('#map-legend-text').style('opacity', 0);
                 d3.selectAll('#map-legend-item').style('opacity', 0);
-                d3.selectAll('#map-legend-rect').attr('width', '68px');
-                d3.selectAll('#map-legend-rect').attr('height', '25px');
-                legend.append('text').text('Legend').attr('x', legend_x - 2).attr('y', legend_y - 2.5).style('font-size', '14px').attr('id', 'map-legend-title');
+                d3.selectAll('#map-legend-rect').transition().duration(250).attr('width', '64px').attr('height', '25px');
+                legend.append('text')
+                    .text('Legend')
+                    .attr('x', legend_x - 2)
+                    .attr('y', legend_y - 2.5)
+                    .style('font-size', '14px')
+                    .attr('id', 'map-legend-title')
+                    .style('opacity', 0)
+                    .transition().duration(250)
+                    .style('opacity', 1);
             }
             else {
-                d3.selectAll('#map-legend-text').style('opacity', 1);
-                d3.selectAll('#map-legend-item').style('opacity', 1);
-                d3.selectAll('#map-legend-rect').attr('width', '185px');
-                d3.selectAll('#map-legend-rect').attr('height', '200px');
+                d3.selectAll('#map-legend-text').transition().duration(250).delay(100).style('opacity', 1);
+                d3.selectAll('#map-legend-item').transition().duration(250).delay(100).style('opacity', 1);
+                d3.selectAll('#map-legend-rect').transition().duration(250).attr('width', '185px').attr('height', '200px');
                 d3.select('#map-legend-title').remove();
             }
         };
@@ -535,7 +536,7 @@ var map = (function (_super) {
         legend.append('rect')
             .attr('x', legend_x - 10)
             .attr('y', legend_y - 20)
-            .attr('width', '68px')
+            .attr('width', '64px')
             .attr('height', '25px')
             .attr('class', 'map-legend-rect')
             .attr('id', 'map-legend-rect')
